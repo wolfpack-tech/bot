101d666fb4c89b2065854a06b3c1254b621ae8ee
koringa wolfgang <118726050+wolfpack-tech@users.noreply.github.com>
2024-10-30 13:24:54 +0000

    bien

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('ready', () => {
 
 // Crée le serveur Express
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware pour traiter les requêtes JSON
 app.use(bodyParser.json());
